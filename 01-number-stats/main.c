d4b6ad48b482e44bb823bb778b78c277376680e4
Çağrı Burak Karen <GITHUB_COMMIT_EMAIL_ADRESN>
2026-07-08 17:46:19 +0300

    commit fix: ai slop. wth is sensor analyzer

--- 01-number-stats/main.c
+++ 01-number-stats/main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 int main(void)
 {
-    int values[100], i;
-    for (i = 0; i < 100; i++) {
-    scanf("%d", &values[i]);
-    printf("%d", values[i]);
-    }
+    int n = 0;
+    printf("how many numbers will you put in\n");
+    scanf("%d", &n);
+    int values[n], i;
+    if (n <= 100){
+        for (i = 0; i <= n-1; i++) {
+            scanf("%d", &values[i]);
+        }
+    }else{
+        printf("The maximum numbers of input can be 100");
+    }    
+    for (i = 0; i <= n-1; i++) {
+        printf("%d", values[i]);
+    }     
     return 0;
 }
 
