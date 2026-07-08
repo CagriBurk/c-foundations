b3c2d5027e4140d7c396fe9ee186a24ab1367dbb
Çağrı Burak Karen <GITHUB_COMMIT_EMAIL_ADRESN>
2026-07-08 20:23:19 +0300

    chor: main function

---
+++ 02-circuit-tool/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+    int choice=0;
+
+    printf("1. Ohm's law calculator\n");
+    printf("2. Series resistance\n");
+    printf("3. Parallel resistance\n");
+    printf("4. Voltage divider\n");
+    printf("5. Exit\n");
+    if(scanf("%d", &choice)!=1){
+        printf("Please enter an integer");
+        return 1;
+    }
+    if(choice<1 || choice>5){
+        printf("Please enter a number between 1-5");
+        return 1;
+    }
+   /*switch(choice){
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+    }*/
+    return 0;
+}
