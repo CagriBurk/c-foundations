f93a1daca522988f468605561dd501b38f97b9e7
Çağrı Burak Karen <GITHUB_COMMIT_EMAIL_ADRESN>
2026-07-08 18:17:10 +0300

    feat: added find_min and find_max functions

--- 01-number-stats/main.c
+++ 01-number-stats/main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+
+int find_min(const int values[], int length){
+    int min_value= values[0];
+    for (int i=1; i<length; i++) {
+        if (min_value > values[i]){
+            min_value=values[i];
+        }
+    };
+    return min_value;
+}
+int find_max(const int values[], int length){
+    int max_value= values[0];
+    for (int i=1; i<length; i++) {
+        if (max_value < values[i]){
+            max_value=values[i];
+        }
+    };
+    return max_value;
+}
+
 int main(void)
 {
     int n = 0;
@@ -11,10 +31,13 @@ int main(void)
         }
     }else{
         printf("The maximum numbers of input can be 100");
-    }    
-    for (i = 0; i <= n-1; i++) {
-        printf("%d", values[i]);
-    }     
+    }
+    int write_min= find_min(values, n); 
+    int write_max= find_max(values, n);       
+    printf("Min value of this numbers is: %d", write_min);
+    printf("Max value of this numbers is: %d", write_max);
     return 0;
 }
+
+
 
