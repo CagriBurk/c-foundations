93a7d71cce0f93aac0060fdcf9cb143e8d5e8d97
Çağrı Burak Karen <192784293+CagriBurk@users.noreply.github.com>
2026-07-09 18:10:43 +0300

    chore: Main task is created.

--- 02-circuit-tool/main.c
+++ 02-circuit-tool/main.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 
 int main(void){
-    int choice=0;
+   
+        int choice=0;
 
-    printf("1. Ohm's law calculator\n");
-    printf("2. Series resistance\n");
-    printf("3. Parallel resistance\n");
-    printf("4. Voltage divider\n");
-    printf("5. Exit\n");
-    if(scanf("%d", &choice)!=1){
-        printf("Please enter an integer");
-        return 1;
+        printf("1. Ohm's law calculator\n");
+        printf("2. Series resistance\n");
+        printf("3. Parallel resistance\n");
+        printf("4. Voltage divider\n");
+        printf("5. Exit\n");
+        while(1){ 
+        if(scanf("%d", &choice)!=1){
+            printf("Please enter an integer");
+            return 1;
+        }
+        if(choice<1 || choice>5){
+            printf("Please enter a number between 1-5");
+            return 1;
+        }
     }
-    if(choice<1 || choice>5){
-        printf("Please enter a number between 1-5");
-        return 1;
-    }
-    printf("Program initializing")
-   /*switch(choice){
-        case 1:
-        break;
-        case 2:
-        break;
-        case 3:
-        break;
-        case 4:
-        break;
-        case 5:
-        break;
-    }*/
+
+    /*switch(choice){
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+        }*/
+
     return 0;
 }
